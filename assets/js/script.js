742eb049197897cbacdb8e766a6a9c01d58ac8b1
JoseEPina <86281203+JoseEPina@users.noreply.github.com>
2023-12-14 18:39:25 +0100

    Added functionality for the form elements to read what is submitted buy the user and assign it to the right option value of the type of task assigned. Then this information is added to the whole list that is displayed on the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
-var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
-    event.preventDefault()
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //* Create the list items  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+
+    //* Create the divs to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //* Give it a class name 
+    taskInfoEl.className = "task-info";
+
+    //* Add HTML content to the div dynamically
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //* Add entire list item to the whole list
     tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl)
 };
 
 formEl.addEventListener("submit", createTaskHandler);
